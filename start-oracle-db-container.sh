50af0af21e4f12343af755ec30eac8ef8d826d00
Vlad Flore <flore.vlad@gmail.com>
2019-12-12 00:35:03 +0100

    added volume to locally save container generated data

--- start-oracle-db-container.sh
+++ start-oracle-db-container.sh
@@ -1,8 +1,12 @@
-docker run -d --name odb-1930 \
--p 1521:1521 -p 5500:5500 \
--e ORACLE_SID=ORCLCDB \
--e ORACLE_PDB=ORCLPDB1 \
--e ORACLE_PWD=welcome1 \
--e ORACLE_CHARACTERSET=AL32UTF8 \
--v /opt/oracle/oradata \
-oracle/database:19.3.0-ee
+docker volume create odb-1930
+docker run -d \
+  --name odb-1930 \
+  -p 1521:1521 -p 5500:5500 \
+  -e ORACLE_SID=ORCLCDB \
+  -e ORACLE_PDB=ORCLPDB1 \
+  -e ORACLE_PWD=welcome1 \
+  -e ORACLE_CHARACTERSET=AL32UTF8 \
+  -v odb-1930:/opt/oracle/oradata \
+  oracle/database:19.3.0-ee
+
+docker logs -f odb-1930
